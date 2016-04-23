a6e5281296847fe5337ed20fa23e82cc1e4301f4
System User <lsuarezzapata5@gmail.com>
2016-04-23 10:10:04 -0500

    Se agregan cambio al api

--- api_saldos/server.js
+++ api_saldos/server.js
@@ -18,3 +18,43 @@ app.post('/postPrueba', function(req,res){
 	console.log(req.body);
 	res.send('Ok :)');
 });
+var pg=require('pg');
+var URL= 'postgres://rsqdgjil:fd9avb8eJFV5RpNpahBwgNs4e1a8Loz-@pellefant.db.elephantsql.com:5432/rsqdgjil';
+
+var client=new pg.Client(URL);
+client.connect(function(err){
+	if(err){
+		return console.log('Error en la conexion a la BD');
+	}
+	console.log('Conexion Realizada con exito');
+	client.end();
+});
+
+app.post('/api/insertar', function(req, res){
+    var cedula = req.body.cedula;
+    var nombre = req.body.nombre;
+    var dinero = req.body.dinero;
+    
+    var queryInsertar = 'INSERT INTO saldos VALUES('
+                + cedula + ', '
+                + '\'' + nombre + '\', '
+                + dinero + ');'
+    console.log(queryInsertar);
+    
+    pg.connect(URL, function(err, client, done){
+        if (err){
+            res.send('Error :(');
+            return console.log('Error de conexión');
+        }
+        client.query(queryInsertar, function(err, result){
+            if(err){
+                res.send('Error :(');
+                client.end();
+                return console.log('Error en el query');
+            }
+            console.log('Se insertó');
+            res.send('OK c:');
+            client.end();
+        });
+    });
+});
